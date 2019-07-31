3acbab8b42b61d4b707a4df65972be16d12eaf4a
juji <39817138+yangshuo0517@users.noreply.github.com>
2019-07-31 16:57:33 +0800

    Update Hello2.java

--- src/com/yangshuo/github/test/Hello2.java
+++ src/com/yangshuo/github/test/Hello2.java
@@ -11,5 +11,6 @@ package com.yangshuo.github.test;
 public class Hello2 {
     public static void main(String[] args) {
         System.out.println("Hello2");
+        System.out.println("Hello3");
     }
 }
