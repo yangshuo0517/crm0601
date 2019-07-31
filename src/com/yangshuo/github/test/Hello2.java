1bb4a70a3d9e660f3ec02cdacca9337b5f129046
yangshuo0517 <916631542@qq.com>
2019-07-31 16:56:22 +0800

    new File

---
+++ src/com/yangshuo/github/test/Hello2.java
@@ -0,0 +1,15 @@
+package com.yangshuo.github.test;
+
+/**
+ * ClassName: Hello2
+ * Description:
+ * date: 2019/7/31 16:55
+ *
+ * @author YS
+ * @since JDK 1.8
+ */
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Hello2");
+    }
+}
